da4fbbd0fce6749276d9ba6d9937ee31f2ae06c8
MehdiREXON <m.ghazanfari1384@gmail.com>
2023-05-24 22:13:16 +0330

    chatGPT has been updated
    has been limited just for 2 channels

--- PY-AI/botai.py
+++ PY-AI/botai.py
@@ -10,15 +10,14 @@ openai.api_key = OPENAI_API_KEY
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name}')
-    print('------')
 
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('ai!'):
-        user_input = message.content[4:]  # Remove the '!' prefix
+    if message.channel.id == 1110871391970000936 or message.channel.id == 1110957540063326338:
+        user_input = message.content
         response = openai.Completion.create(
             engine='text-davinci-003',
             prompt=user_input,
@@ -30,7 +29,11 @@ async def on_message(message):
             frequency_penalty=0.0,
             presence_penalty=0.0
         )
-        await message.channel.send(response.choices[0].text.strip())
+
+        embedVar = discord.Embed(title="Q: " + user_input, color=0x00ff00)
+        embedVar.add_field(name="response from chatGPT :", value=response.choices[0].text.strip(), inline=False)
+        
+        await message.channel.send(embed=embedVar)
 
 
     await bot.process_commands(message)
